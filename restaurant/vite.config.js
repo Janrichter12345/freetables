9b69c8ccaee55d7b485bed5f4041e0b2814f6732
visuhome <visohome8@gmail.com>
2026-01-17 18:54:01 +0100

    update

--- restaurant/vite.config.js
+++ restaurant/vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // ✅ Vercel-Deployment läuft auf Domain-Root
-  base: "/",
+  base: "/partner/",
 });
